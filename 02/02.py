da32299d3ec2809e53915cdcc45b5781fb0d2738
Sirius Sun <siriussun@Siriuss-MacBook-Pro.local>
2019-09-20 21:10:28 +0800

    havent finish yet

--- 02/02.py
+++ 02/02.py
@@ -1,8 +1,15 @@
-## Program 1
-Name=input('Input your name!')
+## Program1
+#1
+Name=input('Input your name:')
 print('Hello,{}!'.format(Name))
 
 ## Program 2
-C=float(input('Input the Celsius!'))
+
+C=float(input('Input the Celsius:'))
+
+while type(C) != float:
+    print('Your input is invalid!')
+    C=float(input('Input a floating number:'))
+
 F=C*1.8+32
 print(F)
