c5e5e540e47d18c80cfc1c9dcb813fdca00222f0
Sirius Sun <siriussun@Siriuss-MacBook-Pro.local>
2019-09-24 18:22:53 +0800

    half-done

--- 02/02.py
+++ 02/02.py
@@ -6,10 +6,5 @@ print('Hello,{}!'.format(Name))
 ## Program 2
 
 C=float(input('Input the Celsius:'))
-
-while type(C) != float:
-    print('Your input is invalid!')
-    C=float(input('Input a floating number:'))
-
 F=C*1.8+32
 print(F)
